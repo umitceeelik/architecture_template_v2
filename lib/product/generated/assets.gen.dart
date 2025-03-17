b72ea76b001509c89ec64e2891502e79567269cc
umitceeelik <umitclk2001@gmail.com>
2025-03-17 23:18:35 +0300

    Environment variables added

---
+++ lib/product/generated/assets.gen.dart
@@ -0,0 +1,27 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetTranslationsGen {
+  const $AssetTranslationsGen();
+
+  /// File path: asset/translations/en.json
+  String get en => 'asset/translations/en.json';
+
+  /// File path: asset/translations/tr.json
+  String get tr => 'asset/translations/tr.json';
+
+  /// List of all assets
+  List<String> get values => [en, tr];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetTranslationsGen translations = $AssetTranslationsGen();
+}
