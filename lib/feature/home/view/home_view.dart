53410f171b3f7de9f26ad35353c74703df6c49d4
umitceeelik <umitclk2001@gmail.com>
2025-03-17 21:55:33 +0300

    Localization completed

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -1,7 +1,12 @@
 import 'package:architecture_template_v2/feature/home/view/mixin/home_view_mixin.dart';
+import 'package:architecture_template_v2/product/init/language/locale_keys.g.dart';
+import 'package:architecture_template_v2/product/init/product_localization.dart';
+import 'package:architecture_template_v2/product/utility/constants/enums/locales.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 part 'widget/home_app_bar.dart';
+
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
@@ -9,9 +14,26 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> with HomeViewMixin{
+class _HomeViewState extends State<HomeView> with HomeViewMixin {
   @override
   Widget build(BuildContext context) {
-    return const _HomeAppBar();
+    return Scaffold(
+      appBar: const _HomeAppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Change language'),
+          ElevatedButton(
+            onPressed: () {
+              ProductLocalization.updateLanguage(
+                context: context,
+                value: Locales.en,
+              );
+            },
+            child: const Text(LocaleKeys.general_button_save).tr(),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/feature/home/view/widget/home_app_bar.dart
+++ lib/feature/home/view/widget/home_app_bar.dart
@@ -1,13 +1,13 @@
 part of '../home_view.dart';
 
-class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget  {
+class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const _HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container();
   }
-  
+
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
